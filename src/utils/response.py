a16fad357d30966e45344eb998127e2abfb13af5
Deniz <denizergunn9@gmail.com>
2026-08-19 09:55:02 +0300

    Refactor response functions in response.py
    Refactor response building functions for TargetDetector and PoseEstimator to improve structure and clarity.

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,46 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.NovaDemopack.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.NovaDemopack.src.models.PackageModel import (
+    PackageModel, PackageConfigs, ConfigExecutor,
+    TargetDetector, TargetDetectorResponse, TargetDetectorOutputs,
+    PoseEstimator, PoseEstimatorResponse, PoseEstimatorOutputs,
+    OutputImage, OutputDetections, OutputData
+)
+
+def build_target_detector_response(context):
+    """Build response model for TargetDetector executor (1 output)."""
+    output_detections = OutputDetections(value=context.output_detections)
+    outputs = TargetDetectorOutputs(outputDetections=output_detections)
+
+    response = TargetDetectorResponse(outputs=outputs)
+    executor = TargetDetector(value=response)
+    config_executor = ConfigExecutor(value=executor)
+    package_configs = PackageConfigs(executor=config_executor)
+
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+    return package.build_model(context)
 
 
-def build_response(context):
-    outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
-    packageConfigs = PackageConfigs(executor=executor)
-    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
-    packageModel = package.build_model(context)
-    return packageModel
+def build_pose_estimator_response(context):
+    """Build response model for PoseEstimator executor (2 outputs)."""
+    output_image = OutputImage(value=context.output_image)
+    output_data = OutputData(value=context.output_data)
+    outputs = PoseEstimatorOutputs(outputImage=output_image, outputData=output_data)
+
+    response = PoseEstimatorResponse(outputs=outputs)
+    executor = PoseEstimator(value=response)
+    config_executor = ConfigExecutor(value=executor)
+    package_configs = PackageConfigs(executor=config_executor)
+
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+    return package.build_model(context)
